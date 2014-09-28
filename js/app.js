90e42af6234230ba0ff4e469c70ac218f1c8e56d
Matthew Wiser <matt.k.wiser@gmail.com>
2014-09-27 21:05:36 -0500

    ajax receiving and sorting done

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function getCalendar() {
   var calendarid = 'qlrklr4bh0rh2gf4k5td996e6c@group.calendar.google.com';
 
   var maxDate = new Date();
-  maxDate.setDate(maxDate.getDate()+14);  
+  maxDate.setDate(maxDate.getDate()+7);  
   maxDate = maxDate.toISOString();
 
   $.ajax({
@@ -31,7 +31,7 @@ function getCalendar() {
       url: encodeURI('https://www.googleapis.com/calendar/v3/calendars/' + calendarid+ '/events?timeMax='+ maxDate  + '&key=' + mykey),
       dataType: 'json',
       success: function (response) {
-        sortCalendarDates(response);
+        receiveCalendarEvents(response);
       },
       error: function (response) {
         console.log('ERROR');
@@ -41,128 +41,116 @@ function getCalendar() {
 }
 
 
-function sortCalendarDates(ajaxResponse){
-  var dates = ajaxResponse.items;
-  var dateComparison = [];
+function receiveCalendarEvents(ajaxResponse){
+  var events = ajaxResponse.items;
+  var eventArray = [];
+  var eventDates = [];
+  for (var i = events.length - 1; i >= 0; i--) {
+    
+    if (events[i].start.date && events[i].status === "confirmed") {    
+    
+      events[i].date = events[i].start.date;
+      eventArray.push(events[i]);
+      eventDates.push(events[i].date);
 
-  for (var i = dates.length - 1; i >= 0; i--) {
-    if (dates[i].start) {
-      if (dates[i].start.date) {
-        
-        // console.log("all day event detected");
-        // console.log(dates[i].summary);
-        // console.log(dates[i].start.date);
-        // console.log(" ");
-        dateComparison.push(dates[i].start.date);
-
-      } else if (dates[i].start.dateTime) {
-        
-        // console.log("timed event detected");
-        // console.log(dates[i].summary);
-        // console.log(dates[i].start.dateTime);
-        // console.log(" ");
-        dateComparison.push(dates[i].start.dateTime);
-
-      } else{
-        
-        console.log(" event without start detected");
-      } 
+    
+    } else if (events[i].start.dateTime && events[i].status === "confirmed") {
+      
+      events[i].date = events[i].start.dateTime;
+      eventArray.push(events[i]);
+      eventDates.push(events[i].date);
+      console.log(events[i].start.dateTime);
+    
     } else {
-      console.log("canceled event detected");
+      console.log("idk wtf this is");
     }
-  };
+  }
+  
+  eventDates = eventDates.sort();
+  sortEvents(eventDates, eventArray);
+}
 
-  console.log(dateComparison.sort()); 
-  _.sortBy(dates)
-  htmlAddCalendar(dates);
+function sortEvents(dateArray, eventArray){
+  var sortedEvents = [];
+  
+  for (var i = dateArray.length - 1; i >= 0; i--) {
+
+    for (var x = eventArray.length - 1; x >= 0; x--) {
+
+      if (eventArray[x].date === dateArray[i]) {
+        sortedEvents.push(eventArray[x]);
+
+      }
+    }
+  }
+  manipulateEventDates(sortedEvents);
 }
 
 
+function manipulateEventDates(events){
+
+  for (var i = events.length - 1; i >= 0; i--) {
+    var time = moment(events[i].date);
+    events[i].date = time.format("ddd, MMM Do");
+  }
+  htmlAddCalendar(events);
+}
+
 function htmlAddCalendar(dates){
 
-  // console.log(ajaxResponse.items);
-  // console.log(" ");
   var $orbitSlider = $("#orbitSlider");
-  var baseDates = [];
-  var baseDescriptions = [];
 
   for (var i = dates.length - 1; i >= 0; i--) {
-    if (dates[i].status==="confirmed") {
-      // console.log(dates[i].summary);
-      // console.log(dates[i].start.dateTime);
-      // console.log(dates[i].start.date);
-      // console.log(dates[i]);
-      // console.log(" ");
 
-      var captionTitle = dates[i].summary;
-      var captionText = dates[i].description;
+    var captionTitle = dates[i].summary;
+    var captionText = dates[i].description;
+    
+    var $li = $("<li>");
+    var $orbitCaption = $("<p>");
+    var $orbitTitle = $("<h4>");
+    var $img = $("<img>");
+    var $date = $("<h6 class='calendarDate'>");
+
+    $date.text(dates[i].date);
+
+    if (captionTitle === "NFL Sunday Ticket") {        
+      $img.attr('src', './img/sundayTicket.jpg');
+    }
+    else if (captionTitle === "Karaoke Wednesday") {
+      $img.attr('src', './img/karaoke.jpg');
+    }
+    else if (captionTitle === "Open Jam") {
+
+      $img.attr('src', './img/openMic.jpg');
+    }
+    else if (captionTitle === "Trivia Sunday") {
+
+      $img.attr('src', './img/triviaThurs.jpg');
+    }   
+    else if (captionTitle === "Live Music at 9") {
+
+      $img.attr('src', './img/liveSat.jpg');
+    }    
+    else if(captionTitle === "Gary Talley Live at 9"){
+      $img.attr('src', './img/liveSat.jpg');
+    }
+    else {
+      $img.attr('src', './img/specialEvent.jpg');
+    }                     
       
-      var containsTitle = _.contains(baseDates, captionTitle);
-      var containsDescription = _.contains(baseDescriptions, captionText);
-      var $li = $("<li>");
-      var $orbitCaption = $("<p>");
-      var $orbitTitle = $("<h4>");
-      var $img = $("<img>");
-      var $date = $("<h6>");
-      $date.attr("class", "calendarDate");
-
-
-      if (containsDescription && containsTitle && !dates[i].recurrence) {
-        // console.log("Date Match Failure Conditions met!!!!!");
-        // console.log(dates[i]);
-        // console.log(" ");
-
-      } else {
-          // console.log("Date Posting Conditions Met!!!!");
-          // console.log(dates[i]);
-          // console.log(" ");
-          if (captionTitle === "NFL Sunday Ticket") {        
-            $img.attr('src', './img/sundayTicket.jpg');
-            $date.text("Sun 09/28");
-          }
-          else if (captionTitle === "Karaoke Wednesday") {
-            $img.attr('src', './img/karaoke.jpg');
-            $date.text("Wed 10/1");
-          }
-          else if (captionTitle === "Open Jam") {
-
-            $img.attr('src', './img/openMic.jpg');
-            $date.text("Fri 10/3");
-          }
-          else if (captionTitle === "Throwback Thursday") {
-
-            $img.attr('src', './img/triviaThurs.jpg');
-            $date.text("Thurs 10/02");
-          }   
-          else if (captionTitle === "Live Music at 9") {
-
-            $img.attr('src', './img/liveSat.jpg');
-            $date.text("Sat 10/4");
-          }    
-          else if(captionTitle === "Gary Talley Live at 9"){
-            $img.attr('src', './img/liveSat.jpg');
-            $date.text("Sat 09/27");
-          }
-          else {
-            $img.attr('src', './img/specialEvent.jpg');
-            
-          }                     
-          
-          // $orbitCaption.attr('class', 'orbit-caption');
-          $orbitCaption.append(captionText);
-          $orbitCaption.attr('class', 'panel');
-
-          $orbitTitle.append(captionTitle);
-          $orbitTitle.attr('class', 'calendarTitle');
-          
-          $li.append($img);
-          $li.append($orbitTitle);
-          $li.append($date);
-          $li.append($orbitCaption);
-          $orbitSlider.append($li);       
-      }  
-    };
-  };
+    $orbitCaption.attr('class', 'panel');
+    $orbitCaption.append(captionText);
+    
+    $orbitTitle.attr('class', 'calendarTitle');
+    $orbitTitle.append(captionTitle);
+    
+    
+    $li.append($img);
+    $li.append($orbitTitle);
+    $li.append($date);
+    $li.append($orbitCaption);
+    $orbitSlider.append($li);       
+  }  
 }
 
 
