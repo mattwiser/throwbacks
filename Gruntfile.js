c76c4616e33f7a244a89560a95ea4dabd8e1e8b4
Matthew Wiser <matt.k.wiser@gmail.com>
2015-05-20 18:35:30 -0500

    updated menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,8 @@ module.exports = function(grunt) {
       server: {
         options: {
           port: 8000,
-          base: './'
+          base: './',
+          keepalive: true
       }
     }
   }
